de590b71bdec9091e3370710d4748da3362e06c2
juanca1096 <jcvj16@gmail.com>
2022-07-07 10:35:10 -0500

    integracion de css y nuevas funciones en js

--- figuras.js
+++ figuras.js
@@ -83,3 +83,61 @@ function areaCirculo (radio) {
 }
 
 console.groupEnd();
+
+  
+//Aqui interactuamos con el HTML
+
+//Funciones para el cuadrado
+function calcularPerimetroCuadrado() {
+    const input = document.getElementById("InputCuadrado");
+    const value = input.value;
+
+    const perimetro = perimetroCuadrado(value);
+    alert("El perimetro de tu cuadradro es " + perimetro + "cm");
+    
+}
+
+function calcularAreaCuadrado() {
+    const input = document.getElementById("InputCuadrado");
+    const value = input.value;  
+
+    const area = areaCuadrado(value);
+    alert(area);
+}
+
+
+
+//Funciones para el triangulo
+function calcularPerimetroTriangulo() {
+    const primerLado = document.getElementById("InputLado1");
+    const segundoLado = document.getElementById("InputLado2");
+    const base = document.getElementById("InputBase");
+    
+    const input1 = primerLado.value;
+    const input2 = segundoLado.value;
+    const input3 = base.value;
+   
+    const perimetro = perimetroTriangulo(input1, input2, input3);
+    alert("El perimetro de tu triangulo es "  + perimetro + "cm");
+
+    console.log(perimetro)
+}
+
+
+
+
+function calcularAreaTriangulo() {
+    const base  = document.getElementById("InputBase2");
+    const altura = document.getElementById("InputAltura");
+
+    const input1 = base.value;
+    const input2 = altura.value;
+
+    console.log(input1);
+    console.log(input2);
+
+    const area = areaTriangulo(input1, input2);
+    alert("El area de tu triangulo es " + area + "cm")
+    
+}
+
